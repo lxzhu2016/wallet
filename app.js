2783beb814e7df7b7ff12b4624368d28c000c837
liangxiong.zhu <lxzhu@outlook.com>
2014-02-09 18:58:08 +0800

    setup ulr pattern /api/:method and handler which resolve this request to correctservice method (action method in MVC terms) we do not have controller here today, but it is easy to add it if it is required. add api.js to work as controller which holds service methods. stage files deleted from linux shell,so it get deleted from git repo.

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
-var express=require('express');
-var http=require('http');
-var path=require('path');
-var app=express();
-app.configure(function(){
-	app.set('port', process.env.PORT||8000);
-	app.set('views',__dirname+'/views');
-	app.set('view engine','jade');
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var api = require('./libs/api');
+var app = express();
+app.configure(function() {
+	app.set('port', process.env.PORT || 8000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
 	app.use(express.favicon());
+	//TODO: how to config so it log to file?
 	app.use(express.logger('dev'));
 	app.use(express.json());
+	//TODO: how to config it to allow url rewrite?
 	app.use(express.urlencoded());
+	app.use(express.query());
+	app.use(express.cookieParser());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
-	app.use(express.static(path.join(__dirname,'pubs')));
+	app.use(express.static(path.join(__dirname, 'pubs')));
 });
-app.configure('development',function(){
+app.configure('development', function() {
 	app.use(express.errorHandler());
 });
-app.get('/',function(req,res){
-	res.render('index',{title:'zhu liangxiong'});
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		title : 'zhu liangxiong'
+	});
 });
-http.createServer(app).listen(app.get('port'),function(error){
-	if(error){
+
+//TODO config express so it treat html as dynamic pages.
+app.get('/:view.html',function(req,res){
+	
+});
+
+app.get('/api/:method', function(req, res) {
+	api.resolve(req.params.method,'get')(req,res);
+});
+
+app.post('/api/:method',function(req,res){
+	api.resolve(req.params.method,'post')(req,res);
+});
+
+http.createServer(app).listen(app.get('port'), function(error) {
+	if (error) {
 		console.log(error);
-	}else{
-		console.log('server is running on port '+app.get('port'));
+	} else {
+		console.log('server is running on port ' + app.get('port'));
 	}
 });
