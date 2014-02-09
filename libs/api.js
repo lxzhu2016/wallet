e726b2d21a8470f3270794904a8f2e6e6ec367da
liangxiong.zhu <lxzhu@outlook.com>
2014-02-09 18:58:08 +0800

    setup ulr pattern /api/:method and handler which resolve this request to correctservice method (action method in MVC terms) we do not have controller here today, but it is easy to add it if it is required. add api.js to work as controller which holds service methods. stage files deleted from linux shell,so it get deleted from git repo.

---
+++ libs/api.js
@@ -0,0 +1,107 @@
+var dao = require('./dao');
+var async = require('async');
+var pri = {
+	http404 : function(it) {
+		return function(req, res) {
+			res.writeHead(404, {
+				'Content-Type' : 'text/plain'
+			});
+			res.end("The method requested does no exist or"
+					+ " is not allowed to request via this http verb.");
+		};
+	},
+	list_methods : function(it) {
+		return function(req, res) {
+			res.end("NOT IMPLEMENTED.");
+		};
+	},
+
+	openDao : function(it, callback) {
+		it.dao.open(null, function(result) {
+			callback(result.error, result);
+		});
+	},
+
+	closeDao : function(it, callback) {
+		it.dao.close(null, function(result) {
+			callback(result.error, result);
+		});
+	}
+};
+
+function login(it) {
+	return function(req, res) {
+		it.dao = dao.createDao();
+		var tasks = {
+			f00 : function(callback) {
+				pri.openDao(it, callback);
+			},
+			f01 : function(callback) {
+				dao.user.login({
+					username : req.body.username,
+					password : req.body.password
+				}, function(result) {
+					callback(result.error, result);
+				});
+			},
+			f02 : function(callback) {
+				pri.closeDao(it, callback);
+			}
+		};
+		var lastCallback = function(error, results) {
+			if (error) {
+				res.locals.username = null;
+				res.locals.isLogin = false;
+				res.writeHead(500, {
+					'Content-Type' : 'text/json'
+				});
+				res.end(JSON.stringify({
+					error : {
+						message : error.message
+					}
+				}));
+			} else {
+				res.locals.username = req.body.username;
+				res.locals.isLogin = true;
+				res.writeHead(200, {
+					'Content-Type' : 'text/json'
+				});
+				res.end(JSON.stringify({
+					ticket : results.f02.ticket
+				}));
+			}
+		};
+		async.series(tasks, lastCallback);
+	};
+}
+
+var pub = {
+	shared : {
+
+	},
+	get : {
+
+	},
+	post : {
+		login : login
+	}
+};
+
+module.exports = {
+	/**
+	 * if method is not specified, list all service methods. if method is
+	 * missing (can not find), return 404. return method handler.
+	 */
+	resolve : function(serviceMethod, httpMethod) {
+		if (!serviceMethod) {
+			return pri.list_methods;
+		} else {
+			var retMethod = null;
+			if (!pub[httpMethod])
+				httpMethod = 'post';
+			retMethod = pub[httpMethod][serviceMethod]
+					|| pub.shared[serviceMethod] || pri.http404;
+			return retMethod({});
+		}
+	}
+};
